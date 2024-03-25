0ccbdbe4358fa297fd6a294747e668ed9ab82598
yadernikoder <161838373+yadernikoder@users.noreply.github.com>
2024-03-25 19:17:56 +0200

    Add files via upload

---
+++ 9.js
@@ -0,0 +1,37 @@
+let MoneyCount = document.getElementById("MoneyCount")
+
+let MyMonet = 0
+
+function add() {
+    
+    MyMonet = MyMonet +1
+    
+    MoneyCount.innerHTML = MyMonet
+
+
+}
+ 
+
+
+function take() {    MyMonet = MyMonet -1
+    
+    MoneyCount.innerHTML = MyMonet}
+
+    function reset() { MyMonet = 0
+    
+        MoneyCount.innerHTML = MyMonet}
+
+
+        function kefel() {    MyMonet = MyMonet *2
+
+            MoneyCount.innerHTML = MyMonet}
+
+            function lehalek() {    MyMonet = MyMonet /2
+
+                MoneyCount.innerHTML = MyMonet}
+
+                function hezka() {    MyMonet = MyMonet **2
+
+                MoneyCount.innerHTML = MyMonet}
+
+
